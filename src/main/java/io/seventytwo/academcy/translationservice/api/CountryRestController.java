18efe9ea6da5ee73cdfa33f1d83e158ff9d27653
Simon Martinelli <simon@72.services>
2018-09-21 10:27:02 +0200

    added lomobok and logging

--- src/main/java/io/seventytwo/academcy/translationservice/api/CountryRestController.java
+++ src/main/java/io/seventytwo/academcy/translationservice/api/CountryRestController.java
@@ -1,5 +1,6 @@
 package io.seventytwo.academcy.translationservice.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Locale;
 
+@Slf4j
 @RestController
 @RequestMapping("/country")
 public class CountryRestController {
 
     @GetMapping("/{isocode}")
     public String getCountryName(@PathVariable String isocode) {
+        log.info("getCountryName({})",  isocode);
+
         Locale.setDefault(new Locale("DE", "ch"));
 
         Locale locale = new Locale("DE", isocode);
--- src/main/java/io/seventytwo/academcy/translationservice/api/LanguageRestController.java
+++ src/main/java/io/seventytwo/academcy/translationservice/api/LanguageRestController.java
@@ -1,5 +1,6 @@
 package io.seventytwo.academcy.translationservice.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Locale;
 
+@Slf4j
 @RestController
 @RequestMapping("/language")
 public class LanguageRestController {
 
     @GetMapping("/{isocode}")
     public String getLanguageName(@PathVariable String isocode) {
+        log.info("getLanguageName({})",  isocode);
+
         Locale.setDefault(new Locale("DE", "ch"));
 
         Locale locale = new Locale(isocode);
