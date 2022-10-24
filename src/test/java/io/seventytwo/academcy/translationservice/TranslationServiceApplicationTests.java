c62a83509776f3163877d017b18c1fc298c590bc
Simon Martinelli <simon@72.services>
2022-10-24 17:05:29 +0200

    Updated Spring Boot version

--- src/test/java/io/seventytwo/academcy/translationservice/TranslationServiceApplicationTests.java
+++ src/test/java/io/seventytwo/academcy/translationservice/TranslationServiceApplicationTests.java
@@ -1,15 +1,12 @@
 package io.seventytwo.academcy.translationservice;
 
 import io.seventytwo.academcy.translationservice.api.CountryRestController;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class TranslationServiceApplicationTests {
 
--- src/test/java/io/seventytwo/academcy/translationservice/api/CountryRestControllerMockTest.java
+++ src/test/java/io/seventytwo/academcy/translationservice/api/CountryRestControllerMockTest.java
@@ -1,10 +1,8 @@
 package io.seventytwo.academcy.translationservice.api;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -14,7 +12,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
-@RunWith(SpringRunner.class)
 public class CountryRestControllerMockTest {
 
     @Autowired
